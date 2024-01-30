37c66383fdab5ccae283427bdd080e351f38e8f9
Nag <nagcloudlab@gmail.com>
2024-01-30 15:48:50 +0530

    PPT

--- play-with-kafka/001-kafka-cluster/3-kafka-topics-lab.md
+++ play-with-kafka/001-kafka-cluster/3-kafka-topics-lab.md
@@ -1,6 +1,5 @@
 
 
-
 create topic
 ```bash
 kafka-topics --create --topic test --bootstrap-server localhost:9092 --replication-factor 1 --partitions 3
@@ -16,8 +15,22 @@ describe topic
 kafka-topics --describe --topic test --bootstrap-server localhost:9092
 ```
 
-
 delete topic
 ```bash
 kafka-topics --delete --topic test --bootstrap-server localhost:9092
+```
+
+alter topic
+```bash
+kafka-topics --alter --topic test --bootstrap-server localhost:9092 --partitions 4
+```
+
+override topic config
+```bash
+kafka-topics --alter --topic test --bootstrap-server localhost:9092 --config retention.ms=1000
+```
+
+reset topic config
+```bash
+kafka-configs --alter --entity-type topics --entity-name test --bootstrap-server localhost:9092 --delete-config retention.ms
 ```
--- play-with-kafka/kafka-java-clients/src/main/java/org/example/ProducerClient.java
+++ play-with-kafka/kafka-java-clients/src/main/java/org/example/ProducerClient.java
@@ -13,17 +13,33 @@ public class ProducerClient {
     public static void main(String[] args) {
 
         Properties properties=new Properties();
-        properties.put("bootstrap.servers","localhost:9092");
+        properties.put("client.id","producer-1");
+        properties.put("bootstrap.servers","localhost:9092,localhost:9093,localhost:9094");
         properties.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
         properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
         //properties.put("partitioner.class","org.example.CustomPartitioner");
+        // safe producer
+        properties.put("acks","all");
+        properties.put("retries",Integer.MAX_VALUE);
+        properties.put("retry.backoff.ms",100);
+        properties.put("max.in.flight.requests.per.connection",1);
+        properties.put("enable.idempotence",true);
+        // high throughput producer
+        properties.put("compression.type","snappy");
+        properties.put("batch.size",16384);
+        properties.put("linger.ms",20);
+        properties.put("buffer.memory",33554432);
+        properties.put("max.block.ms",60000);
+        properties.put("interceptor.classes","org.example.ProducerClientInterceptor");
+
+
 
         KafkaProducer<String,String> kafkaProducer=new KafkaProducer<String, String>(properties);
 
-        for(int i=0;i<10;i++){
+        for(int i=1;i<=1;i++){
             String key="key-"+Integer.toString(i);
             String value="value-"+Integer.toString(i);
-            ProducerRecord<String,String> producerRecord=new ProducerRecord<String, String>("test",key,value);
+            ProducerRecord<String,String> producerRecord=new ProducerRecord<String, String>("topic6",key,value);
             kafkaProducer.send(producerRecord,(recordMetadata, e) -> {
                 if(e==null){
                     logger.info("Received new metadata. \n" +
